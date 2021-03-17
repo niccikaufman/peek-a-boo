57c2ccaa31836db29c0e42db74b98d5e919dee99
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-17 16:00:36 -0700

    add buttons to select different background colors

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,19 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
 
 // So, what's the difference between a class and an ID, except that we use a . to select a class and a # to select an ID? Nothing, really, except that you can only use an ID once on a page, and you can use a class as many times as you want. But by using an ID, you can be clear that you're referring to one particular thing, rather than potentially referring to several.
